436f61777f4512d4fffddcf873f2ef9454d14f92
Luis Arevalo <luisgabrielarevalo@hotmail.com>
2022-08-17 08:38:11 -0500

    Primer commit

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,62 @@
+
+import './myLabel.css';
+
+/**
+ * Esta interfáz se traduce en controles que pueden
+ *  ser manipulados por el usuario via interfaz de
+ *  storybook
+ * 
+ * Los comentarios dentro de la interfaz configuran
+ *  la documentación que será mostrada en la pestaña
+ *  docs de la interfáz de storybook
+ */
+export interface MyLabelProps{
+  /**
+   * Texto que será mostrado por el label
+   */
+  label: string;
+
+  /**
+   * Tamaños del texto a mostrar
+   */
+  size: 'normal' | 'h1' | 'h2' | 'h3';
+
+  /**
+   * El texto será mostrado en mayúscula fija?
+   */
+  allCaps?: boolean;
+
+  /**
+   * Color de fuente del label
+   */
+   color?: 'primary' | 'secondary' | 'tertiary';
+
+   /**
+   * Color de fuente personalizado para el label 
+   */
+   fontColor?: string;
+} 
+
+/**
+ * A pesar de que la interfáz exige que las props sean
+ *  obligatorias, se ponen valores por defecto en el componente
+ *  para que estos sean usados por storybook al momento de 
+ *  renderizar el componente inicialmente
+ */
+
+export const MyLabel = ({
+  allCaps = false,
+  color   = 'primary',
+  label   = 'No label',
+  size    = 'normal',
+  fontColor
+}: MyLabelProps) => {
+  return (
+    <span 
+      className={ `label ${ size } text-${ color }` }
+      style={{ color: fontColor || '' }}
+    >
+      { allCaps ? label.toUpperCase(): label }
+    </span>
+  )
+}
---
+++ src/stories/components/MyLabel.stories.tsx
@@ -0,0 +1,71 @@
+import { MyLabel } from "../../components/MyLabel";
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+
+/**
+ * El title define la "carpeta" en la que se agrupará
+ *  mi componente en el menú lateral del storybook
+ *  ( es la pantalla que sale al ejecutar el script
+ *  de storybook )
+ * 
+ * argTypes personaliza el tipo de control que se 
+ *  mostrará
+ */
+export default {
+  title: 'UI/MyLabel',
+  component: MyLabel,
+  argTypes:{
+    size:      { control: 'select' },
+    color:     { control: 'select' },
+    fontColor: { control: 'color' }
+    // size: { control: 'select', options:['normal', 'h1'] }    // Se pueden personalizar las opciones
+  }
+} as ComponentMeta<typeof MyLabel >
+
+/**
+ * Utilización de nuestro componente. Al tipar de esta manera,
+ *  se estará permitiendo que al componente se le pasen las props
+ *  que está esperando via la interfáz visual de storybook
+ */
+const Template: ComponentStory<typeof MyLabel> = ( args ) => <MyLabel {...args}/>
+
+/**
+ * Crea una copia del Template, rompiendo la referencia
+ *  de javascript a los objetos.
+ * 
+ * Corresponde a las "Páginas" de la historia y debe tener
+ * al menos una.
+ * 
+ * Basic.args corresponde a los valores que estarán seteados
+ *  para cada variante (pagina) de mi componente. Esto hace
+ *  que el control aparezca marcado
+ */
+export const Basic = Template.bind({});
+Basic.args ={
+  size: 'normal',
+  allCaps: false
+}
+
+export const AllCaps = Template.bind({});
+AllCaps.args ={
+  size: 'normal',
+  allCaps: true
+}
+
+export const Secondary = Template.bind({});
+Secondary.args={
+  size: 'normal',
+  color:'secondary'
+}
+
+export const Tertiary = Template.bind({});
+Tertiary.args ={
+  size:'normal',
+  color: 'tertiary'
+}
+
+export const CustomFontColor = Template.bind({});
+CustomFontColor.args = {
+  size: 'h1',
+  fontColor: '#5517ac'
+}
